c66f116a4443e5309cab21893f1ea9bd34ff7c34
Alexis Nicolas <atn4@rice.edu>
2022-06-06 21:35:59 -0400

    added alexis intro

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import exercise from './pictures/exercise.png'
+import alexis from './pictures/alexisPic.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={exercise} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Scroll down to meet the exercise team! OSA Summer 2022
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.notion.so/32edc7143a6546f79647ac4faaa76abc?v=a478ae73ca7d4d2ebdc48236b691b106&p=dddf6628ee02450a91d3de7c2de95754"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check out the Summer Curriculum!
         </a>
       </header>
+      <header className="App-alexis">
+        <h1> 
+          Hi my name is Alexis!
+        </h1>
+        <img src={alexis} alt="yung alexis"/>
+        <p style={{color: "green"}}>
+          I am a rising senior studying CS at Jones!
+        </p>
+      </header>
+
+      {/* Add your intro below this comment! Feel free to style it however you like! */}
+
     </div>
   );
 }
